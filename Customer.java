e1f49f41bb1d868586011a4c79583ff51f37cdbb
adagari <Adam.garibay@gmail.com>
2018-03-27 00:10:47 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,1 +1,26 @@
+public class Customer{
 
+  private String CustNumber;
+  private String tin;
+  private String last;
+  private String first;
+  Private int margin;
+  Private boolean drip;
+  private boolean futures;
+  
+  public Customer(String nbr, String id){
+  }
+  
+  public Customer(String nbr, String id, String lName, String fName,
+                  int lim, boolean reUp, boolean fAuth){
+  }
+  
+  public String describeCustomer(){
+  }
+  
+  public String describeCustomer(){
+  }
+  
+  public String toString(){
+  }
+}
