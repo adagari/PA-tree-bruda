c94467430988eea2936b7ff77f2d15c37df07003
adagari <Adam.garibay@gmail.com>
2018-03-27 00:11:13 -0500

    Update CustomerException.java

--- CustomerException.java
+++ CustomerException.java
@@ -1,10 +1,8 @@
 public class CustomerException extends Exception{
  
- public void CustomerExeption(String msg){
-  
+ public CustomerExeption(String msg){
  }
  
- public void CustomerException(String msg, Throwable cause){
-  
+ public CustomerException(String msg, Throwable cause){
  }
 }
