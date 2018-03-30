9aa54d2db98214bb11f357c31ba765f837523013
adagari <Adam.garibay@gmail.com>
2018-03-30 01:50:28 -0500

    Update Security.java

--- Security.java
+++ Security.java
@@ -1,107 +1,100 @@
 // abstract class Security implements CostBasis
 public abstract class Security implements CostBasis {
-
-    private String custNumber;  // private String
-    private int purchDt;        // private int
-    private double purchPrc;    // private double
-    private double shares;      // private double
-    private String symbol;      // private String
-
-    // constructor five parameters
-    public Security(String nbr, int date, double price,
-                    double qty, String sym) throws SecurityException{
-        setCustNumber(nbr);  // set field custNumber
-        setPurchDT(date);    // set field purchDt
-        setPurchPrc(price);  // set field purchPrc
-        setShares(qty);      // set field shares
-        setSymbol(sym.toUpperCase());      // set field symbol
-    }
-
-    // setCustNumber one String parameter
-    public void setCustNumber(String custNumber) throws SecurityException{
-        if(custNumber.matches("[0-9][0-9][1-9]\\d{3}[1-9]")){
-            this.custNumber = custNumber;
-        }else{
-            throw new SecurityException("Customer number must be 7 digits in length and greater than 10000");
-        }
-    }
-
-    // setPurchDt one int parameter
-  public void setPurchDT(int purchDt) throws SecurityException{
+  
+  private String custNumber;  // private String
+  private int purchDt;        // private int
+  private double purchPrc;    // private double
+  private double shares;      // private double
+  private String symbol;      // private String
+  
+  // constructor five parameters
+  public Security(String nbr, int date, double price,
+                  double qty, String sym) throws SecurityException{
+    this.custNumber = nbr;  // set field custNumber
+    this.purchDt = date;    // set field purchDt
+    this.purchPrc = price;  // set field purchPrc
+    this.shares = qty;      // set field shares
+    this.symbol = sym.toUpperCase();      // set field
     
-    String date = String.valueOf(purchDt);
+    if(!custNumber.matches("\\d{7}") && Integer.parseInt(custNumber) > 10000)
+      throw new SecurityException("Customer number must be 7 digits in length and greater than 10000" + offeredValues());
     
-    if(date.length() != 7){
-      throw new SecurityException("Date must be entered in the format YYYYDDD");
-    }
-    
-    String year = date.substring(0,4);
-    String day = date.substring(4,7);     
-    
-    if(Integer.parseInt(year) >= 1901 && Integer.parseInt(year) <= 9999 &&
-       Integer.parseInt(day) >= 001 && Integer.parseInt(day) <= 365) {
-      this.purchDt = purchDt;
-    }else{
-      throw new SecurityException("Purchase year must be > 1900; Purchase day must be 001 to 365, inclusive.");
-    }
+    String convertDate = String.valueOf(purchDt);
+    if(convertDate.length() != 7)
+      throw new SecurityException("Purchase date must be entered in the format YYYYDDD" + offeredValues());
+    String year = convertDate.substring(0,4);
+    String day = convertDate.substring(4,7);     
+    if(Integer.parseInt(year) < 1901 || Integer.parseInt(year) > 9999 ||
+       Integer.parseInt(day) < 001 || Integer.parseInt(day) > 365)
+      throw new SecurityException("Purchase year must be > 1900; Purchase day must be 001 to 365, inclusive." + offeredValues()); 
   }
-
-    // setPurchPrc one double parameter
-    public void setPurchPrc(double purchPrc) {
-        this.purchPrc = purchPrc;
-    }
-
-    // setShares one double parameter
-    public void setShares(double shares) {
-        this.shares = shares;
-    }
-
-    // setSymbol one String parameter
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
-
-    // getCustNumber returns custNumber field
-    public String getCustNumber() {
-        return custNumber;
-    }
-
-    // getPurchDt returns purchDt field
-    public int getPurchDt() {
-        return purchDt;
-    }
-
-    // getPurchPrc returns purchPrc field
-    public double getPurchPrc() {
-        return purchPrc;
-    }
-
-    // getShares returns shares field
-    public double getShares() {
-        return shares;
-    }
-
-    // getSymbol returns symbol field
-    public String getSymbol() {
-        return symbol;
-    }
-
-    public String offeredValues(){
+  
+  // setCustNumber one String parameter
+  public final void setCustNumber(String custNumber){
+    this.custNumber = custNumber;
+  }
+  
+  // setPurchDt one int parameter
+  public final void setPurchDT(int purchDt){
+    this.purchDt = purchDt;
+  }
+  
+  // setPurchPrc one double parameter
+  public final void setPurchPrc(double purchPrc) {
+    this.purchPrc = purchPrc;
+  }
+  
+  // setShares one double parameter
+  public final void setShares(double shares) {
+    this.shares = shares;
+  }
+  
+  // setSymbol one String parameter
+  public final void setSymbol(String symbol) {
+    this.symbol = symbol;
+  }
+  
+  // getCustNumber returns custNumber field
+  public final String getCustNumber() {
+    return custNumber;
+  }
+  
+  // getPurchDt returns purchDt field
+  public final int getPurchDt() {
+    return purchDt;
+  }
+  
+  // getPurchPrc returns purchPrc field
+  public final double getPurchPrc() {
+    return purchPrc;
+  }
+  
+  // getShares returns shares field
+  public final double getShares() {
+    return shares;
+  }
+  
+  // getSymbol returns symbol field
+  public final String getSymbol() {
+    return symbol;
+  }
+  
+  public String offeredValues(){
     return String.format("%nOffered Values:%n  Customer number: %s%n  Purchase Date: %d%n  Share price:  %f"
-                         + "%n  Number shares:  %f%n  Symbol: %s%n", getCustNumber(), getPurchDt(), getPurchPrc()
-                         , getShares(), getSymbol);
-    }
+                           + "%n  Number shares:  %f%n  Symbol: %s%n", getCustNumber(), getPurchDt(), getPurchPrc()
+                           , getShares(), getSymbol());
+  }
+  
+  // toString returns StringBuilder objInfo
+  public String toString() {
     
-    // toString returns StringBuilder objInfo
-    public String toString() {
-
-        // create StringBuilder object objInfo
-        StringBuilder objInfo = new StringBuilder(String.format("\nThe Security belongs to Customer#  %s;",
-                getCustNumber()));
-
-        objInfo.append(String.format(" %.3f shares of %s were%n", getShares(), getSymbol()));
-        objInfo.append(String.format("purchased on %d for $%.2f per share.%n", getPurchDt(), getPurchPrc()));
-
-        return objInfo.toString();
-    }
+    // create StringBuilder object objInfo
+    StringBuilder objInfo = new StringBuilder(String.format("The Security belongs to Customer#  %s;",
+                                                            getCustNumber()));
+    
+    objInfo.append(String.format(" %.3f shares of %s were%n", getShares(), getSymbol()));
+    objInfo.append(String.format("purchased on %d for $%.2f per share.%n", getPurchDt(), getPurchPrc()));
+    
+    return objInfo.toString();
+  }
 }
