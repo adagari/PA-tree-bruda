8eb30e18548a44be2368cfa89637c2c059d0b1f5
M Folmar <folmar.meagan@yahoo.com>
2018-04-04 11:00:02 -0500

    Update Security.java

--- Security.java
+++ Security.java
@@ -1,103 +1,158 @@
-// abstract class Security implements CostBasis
-public abstract class Security implements CostBasis {
-  
-  private String custNumber;  // private String
-  private int purchDt;        // private int
-  private double purchPrc;    // private double
-  private double shares;      // private double
-  private String symbol;      // private String
-  
-  // constructor five parameters
-  public Security(String nbr, int date, double price,
-                  double qty, String sym) throws SecurityException{
-    this.custNumber = nbr;  // set field custNumber
-    this.purchDt = date;    // set field purchDt
-    this.purchPrc = price;  // set field purchPrc
-    this.shares = qty;      // set field shares
-    this.symbol = sym.toUpperCase();      // set field
+public abstract class Security implements CostBasis //abstract class Security implements CostBasis
+{
+  private String custNumber; //unique ID for customer
+  private int purchDt; //purchase date of Security
+  private double purchPrc; //purchase price in dollars
+  private double shares; //number of shares purchased
+  private String symbol; //market symbol of Security
+
+  public Security(String nbr, int date, double price, double qty, String sym) throws SecurityException
+  {
+    /*Assign variables*/
     
+    this.custNumber = nbr;  
+    this.purchDt = date;    
+    this.purchPrc = price;  
+    this.shares = qty;      
+    this.symbol = sym.toUpperCase();      
+    
+    /*throw exception if customer number is not 7 digits long and is less than 10000*/
+
     if(!custNumber.matches("\\d{7}") || Integer.parseInt(custNumber) < 10000)
+    {
       throw new SecurityException("Customer number must be 7 digits in length and greater than 10000" + offeredValues());
     
+    }//end if statement
+
     String convertDate = String.valueOf(purchDt);
-    if(convertDate.length() != 7)
-      throw new SecurityException("Purchase date must be entered in the format YYYYDDD" + offeredValues());
-    String year = convertDate.substring(0,4);
-    String day = convertDate.substring(4,7);     
-    if(Integer.parseInt(year) < 1901 || Integer.parseInt(year) > 9999 ||
-       Integer.parseInt(day) < 001 || Integer.parseInt(day) > 365)
-      throw new SecurityException("Purchase year must be > 1900; Purchase day must be 001 to 365, inclusive." + offeredValues()); 
-  }
-  
-  // setCustNumber one String parameter
-  public final void setCustNumber(String custNumber){
-    this.custNumber = custNumber;
-  }
-  
-  // setPurchDt one int parameter
-  public final void setPurchDT(int purchDt){
-    this.purchDt = purchDt;
-  }
-  
-  // setPurchPrc one double parameter
-  public final void setPurchPrc(double purchPrc) {
-    this.purchPrc = purchPrc;
-  }
-  
-  // setShares one double parameter
-  public final void setShares(double shares) {
-    this.shares = shares;
-  }
-  
-  // setSymbol one String parameter
-  public final void setSymbol(String symbol) {
-    this.symbol = symbol;
-  }
-  
-  // getCustNumber returns custNumber field
-  public final String getCustNumber() {
-    return custNumber;
-  }
-  
-  // getPurchDt returns purchDt field
-  public final int getPurchDt() {
-    return purchDt;
-  }
-  
-  // getPurchPrc returns purchPrc field
-  public final double getPurchPrc() {
-    return purchPrc;
-  }
-  
-  // getShares returns shares field
-  public final double getShares() {
-    return shares;
-  }
-  
-  // getSymbol returns symbol field
-  public final String getSymbol() {
-    return symbol;
-  }
-  
-  public String offeredValues(){ 
     
+    /*throw exception if purchase date is not 7 digits long*/
+
+    if(convertDate.length() != 7)
+    {
+      throw new SecurityException("Purchase date must be entered in the format YYYYDDD" + offeredValues());
+
+    }//end if statement
+    
+    String year = convertDate.substring(0,4);
+
+    String day = convertDate.substring(4,7);     
+    
+    /*throw exception if purchase year is earlier than 1900 or greater than 9999 or if purchase day is not
+     * between 001 and 365*/
+
+    if(Integer.parseInt(year) < 1901 || Integer.parseInt(year) > 9999 || 
+       Integer.parseInt(day) < 001 || Integer.parseInt(day) > 365)
+    {
+      throw new SecurityException("Purchase year must be > 1900; Purchase day must be 001 to 365, inclusive." + offeredValues()); 
+    
+    }//end if statement
+  
+  }//end constructor
+  
+  /*overshadowing instance variable custNumber by local variable*/
+
+  public final void setCustNumber(String custNumber)
+  {
+    this.custNumber = custNumber;
+
+  }//end public final void setCustNumber
+  
+  /*overshadowing instance variable purchDt by local variable*/
+
+  public final void setPurchDT(int purchDt)
+  {
+    this.purchDt = purchDt;
+
+  }//end public final void setPurchDt
+  
+  /*overshadowing instance variable purchPrc by local variable*/
+
+  public final void setPurchPrc(double purchPrc) 
+  {
+    this.purchPrc = purchPrc;
+
+  }//end public final void setPurchPrc
+  
+  /*overshadowing instance variable shares by local variable*/
+
+  public final void setShares(double shares) 
+  {
+    this.shares = shares;
+
+  }//end public final void setShares
+  
+  /*overshadowing instance variable symbol by local variable*/  
+
+  public final void setSymbol(String symbol) 
+  {
+    this.symbol = symbol;
+
+  }//end public final void setSymbol
+  
+  /*returning value of custNumber*/
+
+  public final String getCustNumber() 
+  {
+    return custNumber;
+
+  }//end public final String getCustNumber
+  
+  /*returning value of purchDt*/
+
+  public final int getPurchDt() 
+  {
+    return purchDt;
+
+  }//end public final int getPurchDt
+  
+  /*returning value of purchPrc*/
+
+  public final double getPurchPrc() 
+  {
+    return purchPrc;
+
+  }//end public final double getPurchPrc
+  
+  /*returning value of shares*/
+
+  public final double getShares() 
+  {
+    return shares;
+
+  }//end public final double getShares
+  
+  /*returning value of symbol*/
+
+  public final String getSymbol() 
+  {
+    return symbol;
+
+  }//end public final String getSymbol
+  
+  /*returning customer info*/
+
+  public String offeredValues()
+  { 
     return String.format("%nOffered Values:%n  Customer number: %s%n"
                            + "  Purchase Date: %d%n  Share price:  %f"
-                           + "%n  Number shares:  %f%n  Symbol: %s%n",
-                         getCustNumber(), getPurchDt(), getPurchPrc(),
+                           + "%n  Number shares:  %f%n  Symbol: %s%n", getCustNumber(), getPurchDt(), getPurchPrc(),
                          getShares(), getSymbol());
-  }
+
+  }//end public String offeredValues
   
-  // toString returns StringBuilder objInfo
-  public String toString() {
-    
-    // create StringBuilder object objInfo
-    StringBuilder objInfo = new StringBuilder(String.format("The Security belongs to Customer#  %s;",
-                                                            getCustNumber()));
-    
+
+  /*returns StringBuilder objInfo*/
+
+  public String toString() 
+  { 
+    StringBuilder objInfo = new StringBuilder(String.format("The Security belongs to Customer#  %s;", getCustNumber()));
+
     objInfo.append(String.format(" %.3f shares of %s were%n", getShares(), getSymbol()));
     objInfo.append(String.format("purchased on %d for $%.2f per share.%n", getPurchDt(), getPurchPrc()));
-    
+
     return objInfo.toString();
-  }
-}
+
+  }//end public String toString
+
+}//end public abstract class Security implements CostBasis
