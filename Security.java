aeeed673bf9941c16d5cdfa78f021c3174ad5d69
M Folmar <folmar.meagan@yahoo.com>
2018-04-05 12:03:59 -0500

    Update Security.java

--- Security.java
+++ Security.java
@@ -15,13 +15,13 @@ public abstract class Security implements CostBasis //abstract class Security im
     this.purchPrc = price;  
     this.shares = qty;      
     this.symbol = sym.toUpperCase();      
-    
+
     /*throw exception if customer number is not 7 digits long and is less than 10000*/
 
     if(!custNumber.matches("\\d{7}") || Integer.parseInt(custNumber) < 10000)
     {
-      throw new SecurityException("Customer number must be 7 digits in length and greater than 10000" + offeredValues());
-    
+      throw new SecurityException("Customer number must be 7 digits long and greater than 0010000." + offeredValues());
+
     }//end if statement
 
     String convertDate = String.valueOf(purchDt);
@@ -31,13 +31,12 @@ public abstract class Security implements CostBasis //abstract class Security im
     if(convertDate.length() != 7)
     {
       throw new SecurityException("Purchase date must be entered in the format YYYYDDD" + offeredValues());
-
+      
     }//end if statement
-    
-    String year = convertDate.substring(0,4);
 
+    String year = convertDate.substring(0,4);
     String day = convertDate.substring(4,7);     
-    
+
     /*throw exception if purchase year is earlier than 1900 or greater than 9999 or if purchase day is not
      * between 001 and 365*/
 
@@ -45,19 +44,19 @@ public abstract class Security implements CostBasis //abstract class Security im
        Integer.parseInt(day) < 001 || Integer.parseInt(day) > 365)
     {
       throw new SecurityException("Purchase year must be > 1900; Purchase day must be 001 to 365, inclusive." + offeredValues()); 
-    
+
     }//end if statement
-  
+
   }//end constructor
-  
+
   /*overshadowing instance variable custNumber by local variable*/
 
   public final void setCustNumber(String custNumber)
   {
     this.custNumber = custNumber;
 
-  }//end public final void setCustNumber
-  
+  }//end public final void setCustNumber 
+
   /*overshadowing instance variable purchDt by local variable*/
 
   public final void setPurchDT(int purchDt)
@@ -65,7 +64,7 @@ public abstract class Security implements CostBasis //abstract class Security im
     this.purchDt = purchDt;
 
   }//end public final void setPurchDt
-  
+
   /*overshadowing instance variable purchPrc by local variable*/
 
   public final void setPurchPrc(double purchPrc) 
@@ -73,7 +72,8 @@ public abstract class Security implements CostBasis //abstract class Security im
     this.purchPrc = purchPrc;
 
   }//end public final void setPurchPrc
-  
+
+
   /*overshadowing instance variable shares by local variable*/
 
   public final void setShares(double shares) 
@@ -81,7 +81,7 @@ public abstract class Security implements CostBasis //abstract class Security im
     this.shares = shares;
 
   }//end public final void setShares
-  
+
   /*overshadowing instance variable symbol by local variable*/  
 
   public final void setSymbol(String symbol) 
@@ -89,7 +89,7 @@ public abstract class Security implements CostBasis //abstract class Security im
     this.symbol = symbol;
 
   }//end public final void setSymbol
-  
+
   /*returning value of custNumber*/
 
   public final String getCustNumber() 
@@ -97,7 +97,7 @@ public abstract class Security implements CostBasis //abstract class Security im
     return custNumber;
 
   }//end public final String getCustNumber
-  
+
   /*returning value of purchDt*/
 
   public final int getPurchDt() 
@@ -105,7 +105,7 @@ public abstract class Security implements CostBasis //abstract class Security im
     return purchDt;
 
   }//end public final int getPurchDt
-  
+
   /*returning value of purchPrc*/
 
   public final double getPurchPrc() 
@@ -113,7 +113,7 @@ public abstract class Security implements CostBasis //abstract class Security im
     return purchPrc;
 
   }//end public final double getPurchPrc
-  
+
   /*returning value of shares*/
 
   public final double getShares() 
@@ -121,7 +121,7 @@ public abstract class Security implements CostBasis //abstract class Security im
     return shares;
 
   }//end public final double getShares
-  
+
   /*returning value of symbol*/
 
   public final String getSymbol() 
@@ -129,18 +129,17 @@ public abstract class Security implements CostBasis //abstract class Security im
     return symbol;
 
   }//end public final String getSymbol
-  
+
   /*returning customer info*/
 
   public String offeredValues()
   { 
-    return String.format("%nOffered Values:%n  Customer number: %s%n"
-                           + "  Purchase Date: %d%n  Share price:  %f"
-                           + "%n  Number shares:  %f%n  Symbol: %s%n", getCustNumber(), getPurchDt(), getPurchPrc(),
+    return String.format("%nOffered values:%n  Customer number: %s%n"
+                           + "  Purchase date: %d%n  Share price: %f"
+                           + "%n  Number shares: %f%n  Symbol: %s%n", getCustNumber(), getPurchDt(), getPurchPrc(),
                          getShares(), getSymbol());
 
   }//end public String offeredValues
-  
 
   /*returns StringBuilder objInfo*/
 
