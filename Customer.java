c9d269e5d2aef03f66ddfa2c1bd85aac8c76befc
M Folmar <folmar.meagan@yahoo.com>
2018-04-05 11:56:43 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -27,49 +27,49 @@ public class Customer
     this.margin = lim;
     this.drip = reUp;
     this.futures = fAuth;
-    
+
     /*throwing exception if customer number isn't 7 digits and is less than 10000*/
 
     if(!custNumber.matches("\\d{7}") || Integer.parseInt(custNumber) < 10000) 
     {
       throw new CustomerException("Customer number must be 7 digits in length and greater than 10000." + toString());
-    
-    }//end if statement
-    
-    /*throw exception if tax ID is not 9 digits long*/
-   
-    if(!tin.matches("\\d{9}"))
-    {
-      throw new CustomerException("Tax Identifier must be 9 digits in length." + toString());
-      
+
     }//end if statement
 
-    /*throw exception if last name is invalid*/
-    
+    /*throw exception if tax ID is not 9 digits long*/
+
+    if(!tin.matches("\\d{9}"))
+    {
+      throw new CustomerException("Tax Identifier must be 9 digits in length." + toString()); 
+
+    }//end if statement
+
+    /*throw exception if last name is invalid*/    
+
     if(!last.matches("[a-zA-z]+(['-][a-zA-z]+)*"))
     {
       throw new CustomerException("Last name must not be null or blank." + toString());
-      
+
     }//end if statement
-    
+
     /*throw exception if first name is invalid*/    
 
     if(!first.matches("[A-Z][a-zA-Z]*"))
     {
       throw new CustomerException("First name must not be null or blank." + toString());
-
+      
     }//end if statement
-    
-    /*throw exception if margin is less than 0*/
 
+    /*throw exception if margin is less than 0*/
+    
     if(margin < 0)
     {
       throw new CustomerException("The margin value must be zero or greater." + toString()); 
-  
+      
     }//end if statement
-    
+
   }//end constructor
-  
+
     /*overshadowing instance variable custNumber by local variable*/
 
     public final void setCustNumber(String custNumber)
@@ -77,7 +77,7 @@ public class Customer
       this.custNumber = custNumber;
 
     }//end public final void setCustNumber(String custNumber)
-    
+
     /*overshadowing instance variable tin by local variable*/
 
     public final void setTin(String tin)
@@ -85,7 +85,7 @@ public class Customer
       this.tin = tin;
 
     }//end public final void setTin
-    
+
     /*overshadowing instance variable last by local variable*/
 
     public final void setLast(String last)
@@ -93,7 +93,7 @@ public class Customer
       this.last = last;
 
     }//end public final void setLast
-    
+
     /*overshadowing instance variable first by local variable*/
 
     public final void setFirst(String first)
@@ -101,7 +101,7 @@ public class Customer
       this.first = first;
 
     }//end public final void setFirst   
-    
+
     /*overshadowing instance variable margin by local variable*/
 
     public final void setMargin(int margin)
@@ -109,7 +109,7 @@ public class Customer
       this.margin = margin;
 
     }//end public final void setMargin
-    
+
     /*overshadowing instance variable drip by local variable*/
 
     public final void setDrip(boolean drip)
@@ -117,7 +117,7 @@ public class Customer
       this.drip = drip;
 
     }//end public final void setDrip
-    
+
     /*overshadowing instance variable futures by local variable*/
 
     public final void setFutures(boolean futures)
@@ -133,15 +133,15 @@ public class Customer
       return custNumber;
 
     }//end public final String getCustNumber
-    
+
     /*returning value of tin*/
 
     public final String getTin()
     {
       return tin;
-
+      
     }//end public final String getTin
-    
+
     /*returning value of last*/
 
     public final String getLast()
@@ -149,7 +149,7 @@ public class Customer
       return last;
 
     }//end public final String getLast
-    
+
     /*returning value of first*/
 
     public final String getFirst()
@@ -157,7 +157,7 @@ public class Customer
       return first;
 
     }//end public final String getFirst
-    
+
     /*returning value of margin*/
 
     public final int getMargin()
@@ -165,7 +165,7 @@ public class Customer
       return margin;
 
     }//end public final int getMargin
-    
+
     /*returning value of drip*/
 
     public final boolean getDrip()
@@ -173,7 +173,7 @@ public class Customer
       return drip;
 
     }//end public final boolean getDrip
-    
+
     /*returning value of futures*/
 
     public final boolean getFutures()
@@ -181,24 +181,24 @@ public class Customer
       return futures;
 
     }//end public final boolean getFutures 
-    
+
     /*returning object info using append and StringBuilder*/
 
     public String describeCustomer()
     {
       StringBuilder objInfo = new StringBuilder(String.format("Customer %s, %s %s.%n  The account carries" 
-                                                                "a margin limit of $%d.00.", getCustNumber(), 
+                                                               + "a margin limit of $%d.00.", getCustNumber(), 
                                                               getFirst(), getLast(), getMargin()));
-      
+
       /*series of if/else statements using append determining if customer can reinvest dividends or 
        * trading in future contracts*/
-      
+
       if(getDrip() == true)
       {
         objInfo.append("\n  The account will reinvest dividends as recieved.");
 
       }//end if statement
-      
+
       else
       {
         objInfo.append("\n  The account will not reinvest dividends as recieved.");
@@ -220,7 +220,7 @@ public class Customer
       return objInfo.toString(); //returning object info
 
     }//end public String describeCustomer
-    
+
     /*returning String object that prints out information about customer*/
 
     public String toString()
@@ -231,5 +231,5 @@ public class Customer
                                getFirst(), getMargin(), getDrip(), getFutures());
 
     }//end public String toString
-
+  
   }//end public class Customer
